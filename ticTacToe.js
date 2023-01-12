29399f827dc012b84cb94314bd4c2ada0720c69e
JaimePerales <jaimeperales123@gmail.com>
2023-01-12 09:40:00 -0600

    Create gameboard

--- ticTacToe.js
+++ ticTacToe.js
@@ -9,3 +9,53 @@ const gameboard = (() => {
 
 // Factory to create player objects
 const playerFactory = (name, symbol) => ({ name, symbol });
+
+// Handles all display needs
+const displayController = (() => {
+    const body = document.querySelector('body');
+
+    // Creates Game Title text
+    const createGameTitleText = () => {
+        const gameTitleTextDiv = document.createElement('div');
+        gameTitleTextDiv.classList.add('game-title-text');
+
+        const gameTitleText = document.createElement('p');
+        gameTitleText.textContent = 'TIC-TAC-TOE';
+
+        gameTitleTextDiv.appendChild(gameTitleText);
+        body.appendChild(gameTitleTextDiv);
+    }
+
+    const createGameBoard = () => {
+        const gameboardDiv = document.createElement('div');
+        gameboardDiv.classList.add('gameboard-div');
+
+        // eslint-disable-next-line no-plusplus
+        for (let i = 0; i < 3; i++) {
+            const gameRow = document.createElement('div');
+            gameRow.classList.add(`row-${i}`);
+            // eslint-disable-next-line no-plusplus
+            for (let j = 0; j < 3; j++) {
+                const gameCell = document.createElement('div');
+                gameCell.classList.add(`gamecell-${j}-${i}`);
+                gameCell.addEventListener('click', () => {
+
+                });
+                gameRow.appendChild(gameCell);
+            }
+            gameboardDiv.appendChild(gameRow);
+        }
+        body.appendChild(gameboardDiv);
+    }
+
+    // Runs all setup methods
+    const initializeView = () => {
+        createGameTitleText();
+        createGameBoard();
+    }
+    return {
+        initializeView
+    };
+})();
+
+displayController.initializeView();
